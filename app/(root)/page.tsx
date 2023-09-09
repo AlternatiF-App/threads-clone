423afff85c31b428b2e60fe4e28d781b16224a52
AlternatiF-App <fananiapps@gmail.com>
2023-09-09 11:28:00 +0700

    Add onboarding page with integrate mongo and add create thread function and map it in home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,41 @@
+import ThreadCard from '@/components/cards/ThreadCard'
+import { fetchPosts } from '@/lib/actions/thread.action'
+import { currentUser } from '@clerk/nextjs'
 
-const Home = () => {
+const Home = async () => {
+  const result = await fetchPosts(1, 30)
+  const user = await currentUser()
+
   return (
-    <main>
-      Home
-    </main>
+    <>
+      <h1 className='head-text text-left'>
+        Home
+      </h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {
+          result.posts.length === 0
+            ? <p className='no-results'>No threads found</p>
+            : <>
+                {
+                  result.posts.map((post) => (
+                    <ThreadCard
+                      key={post._id}
+                      id={post._id}
+                      currentUserId={user?.id || ''}
+                      parentId={post.parentId}
+                      content={post.text}
+                      author={post.author}
+                      community={post.community}
+                      createdAt={post.createdAt}
+                      comments={post.children}
+                    />
+                  ))
+                }
+              </>
+        }
+      </section>
+    </>
   )
 }
 
